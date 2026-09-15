e1fcec7eb322c677d6eaf44f9630cb7110927af5
yehia ayman <yehiaelassy3@icloud.com>
2026-09-15 04:15:10 +0300

    Update todos.js

--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -5,7 +5,14 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api/todos'
 });
 
-export const fetchTodos = () => api.get('/').then(res => res.data);
+export const fetchTodos = (filter) => {
+  const params = {};
+  if (filter === 'active') params.done = false;
+  if (filter === 'done') params.done = true;
+  // 'all' or undefined -> no params, server returns everything
+
+  return api.get('/', { params }).then(res => res.data);
+};
 
 export const createTodo = (title) =>
   api.post('/', { title }).then(res => res.data);
